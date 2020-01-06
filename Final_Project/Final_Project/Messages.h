ea00897dcb842be34eeee0f21534acdfea74cbae
osher sargani <sarganio@post.bgu.ac.il>
2020-01-06 16:03:04 +0200

    no compilation errors!

--- Final_Project/Final_Project/Messages.h
+++ Final_Project/Final_Project/Messages.h
@@ -1,10 +1,14 @@
 #pragma once
 #include<cstdint>
-#include<WinSock2.h>
+
 #define KEY_LEN 32
 #define SEQ_LEN 4
 #define RECONSTRUCT_LEN 64
+#define NUM_OF_PARTIES 3
 
+#define BASE_PORT 8000
+#define BASE_IP "192.168.0."
+//enum ipAddresses{'192.168.0.' };
 enum types{SEQ = 0,KEY,RECONSTRUCT};
 
 class Message {
@@ -34,16 +38,18 @@ public:
 	void setData(const char* dataPtr) {
 		memcpy(_data + 1, dataPtr, _size);
 	}
-	bool sendMsg(SOCKET& sock, Message& msg) {
+	/*bool sendMsg(SOCKET& sock, Message& msg) {
 		short size = msg._size;
-		int wasSent = send(sock, _data,size , 0);
+		int wasSent = 
+			
+			(sock, _data,size , 0);
 		return wasSent == size;
 	}
 	~Message(){
 		if(_data)
 			delete[] _data;
 		_data = nullptr;
-	}
+	}*/
 };
 /*
 #pragma pack(1)
--- Final_Project/Final_Project/Party.h
+++ Final_Project/Final_Project/Party.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <vector>
-#include <WinSock2.h>
-#include <Windows.h>
+#include "Messages.h"
+//#include <WinSock2.h>
+//#include <Windows.h>
 
 using std::vector;
 using std::pair;
@@ -9,10 +10,10 @@ using std::pair;
 class Party
 {
 private:
-	short id;//unique identifier, lowest ip receive id = 1. 
-	long input;//secret input 
-	vector<SOCKET> sockets;//socket for all TCP connection. sockets[0] is a UDP soket
-	vector<pair<long, long>> shares;//index of vector is the id of input's party
+	short _id;//unique identifier, lowest ip receive id = 1. 
+	long _input;//secret input 
+	//vector<SOCKET> _sockets;//socket for all TCP connection. sockets[0] is a UDP soket
+	vector<pair<long, long>> _shares;//index of vector is the id of input's party
 public:
 	void connectToAllParties();
 
