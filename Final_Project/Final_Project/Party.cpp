f683fa9119b4ccc1236b64190b66f8da48eedfe1
osher sargani <sarganio@post.bgu.ac.il>
2020-01-06 16:30:39 +0200

    more cosmetic changes

--- Final_Project/Final_Project/Party.cpp
+++ Final_Project/Final_Project/Party.cpp
@@ -1,14 +1,20 @@
 #include <string>
 #include <iostream>
 #include "Party.h"
+#include "TcpClient.h"
+#include "TcpServer.h"
 
 using std::string;
 using std::cout;
+using stk::TcpClient;
+using stk::TcpServer;
 void Party::connectToAllParties() {
 	short idToConnect = (_id + 1) % NUM_OF_PARTIES;
-	string destIP = BASE_IP + std::to_string(idToConnect);
+	string toIP = BASE_IP + std::to_string(idToConnect);
 	string myIP = BASE_IP + std::to_string(_id);
 
-	short destPort = BASE_PORT + idToConnect, myPort = BASE_PORT + _id;
-
+	short toPort = BASE_PORT + idToConnect, myPort = BASE_PORT + _id;
+	
+	TcpClient to = TcpClient(myPort);
+	to.connect(toPort);
 }
