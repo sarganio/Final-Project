34a13d2963658e3e886f409800674cb92419c949
osher sargani <sarganio@post.bgu.ac.il>
2020-08-11 13:52:24 +0300

    fixed include connection problem in TcpClient

--- Final_Project/Final_Project/TcpClient.cpp
+++ Final_Project/Final_Project/TcpClient.cpp
@@ -1,11 +1,12 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include "TcpClient.h"
 #include "Messages.h"
 #include "Helper.h"
-#include <WS2tcpip.h>
 #include <cstring>
 #include <sstream>
 #include <iostream>
 #include <thread>
+
 #define IP_INDEX 1
 using std::thread;
 TcpClient::TcpClient(string myIP,unsigned short myPort, unsigned short hostPort, std::string hostIp, Message* mess,bool& isConnected) :TcpSocket(-1, hostPort)//, mutex& m):TcpSocket(-1,myPort)
@@ -33,8 +34,8 @@ int TcpClient::connect(unsigned short hostPort, std::string hostname, Message* m
 
 	sa.sin_port = htons(hostPort); // port that server will listen to
 	sa.sin_family = AF_INET;   // must be AF_INET
-	sa.sin_addr.s_addr = InetPton(AF_INET, hostname.c_str(), &sa.sin_addr.s_addr);    // the IP of the server
-	//sa.sin_addr.s_addr = inet_addr(hostname.c_str());    // the IP of the server
+	//sa.sin_addr.s_addr = InetPton(AF_INET, hostname.c_str(), &sa.sin_addr.s_addr);    // the IP of the server
+	sa.sin_addr.s_addr = inet_addr(hostname.c_str());    // the IP of the server
 
 	int status,i = 0;
 	while (status = ::connect(_socket, (struct sockaddr*) & sa, sizeof(sa))) {
