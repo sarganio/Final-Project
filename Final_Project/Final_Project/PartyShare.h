6a42dee66c86f32f14a0fa1353263caef9e223e2
osher sargani <sarganio@post.bgu.ac.il>
2020-08-23 18:05:52 +0300

    transformation to PartyShare almost complete

--- Final_Project/Final_Project/PartyShare.h
+++ Final_Project/Final_Project/PartyShare.h
@@ -11,8 +11,11 @@ public:
 	PartyShare(Share* prevOutput, Party* p) :Share(*prevOutput) {
 		_owner = p;
 	}
+	int correlatedRandomness(Party& p) const;	//calculate correlated randomness as described in pepare.
+	Share calculateOutputMull(MultiplicationGate<Share*>& g);
 	Party* getParty()const { return _owner; }
 	friend Share operator*(Share& left, PartyShare& right) {//TODO----------------------------
+		
 		return Share();
 	}
 };
